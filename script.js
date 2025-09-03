df173e7e42b8ea364587cd7001fb276c75d97b8b
Vishal Wani <93905384+Vishalwani123@users.noreply.github.com>
2025-09-03 18:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ squares.forEach((square, index) => {
     square.addEventListener('mouseenter', () => {
         squares.forEach((sq, i) => {
             if (i !== index) {
-                sq.style.backgroundColor = 'rgb(111, 78, 55)'; 
+                sq.style.backgroundColor = '#6F4E37';
             }
         });
     });
 
     square.addEventListener('mouseleave', () => {
-        squares.forEach(sq => {
-            sq.style.backgroundColor = 'rgb(230, 230, 250)'; 
+		squares.forEach((sq, i) => {
+            if (i !== index) {
+                sq.style.backgroundColor = '#E6E6FA';
+            }
         });
     });
 });
